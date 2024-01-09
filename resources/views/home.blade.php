d1f59a483d628d945c3e42c86183eb394536178d
iaio998 <depalatisiacopo@gmail.com>
2024-01-09 18:08:09 +0100

    Removed card from home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,17 @@
         <div class="container">
             <div class="row">
                 @foreach ($comics as $comic)
-                @include('partials.card')
+                <div class="col-2">
+                    <div>
+                        <a href="{{route('comics.show', $comic->id)}}">
+                            <img class="my-img" src="{{$comic->thumb}}" alt="{{$comic->title}}" />
+                            <div class="text-white text-center">
+                                <h2 class="fs-5">{{$comic->title}}</h2>
+                                <p>{{$comic->series}}</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
                 @endforeach
             </div>
         </div>
