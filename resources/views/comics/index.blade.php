53c734e85ac906d8aa5860889cb95d70a70aa493
iaio998 <depalatisiacopo@gmail.com>
2024-01-10 16:13:39 +0100

    Added reset button

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,10 +10,15 @@
                 <a class="my-btn" href="{{route('comics.create')}}">
                     Add new
                 </a>
-                <form action="{{route('comics.index')}}" method="GET" class="my-2">
-                    <input class="my-input" type="text" name="search" placeholder="Search">
-                    <button class="btn btn-primary" type="submit">Search</button>
-                </form>
+                <div class="d-flex gap-2">
+                    <form action="{{route('comics.index')}}" method="GET" class="my-2">
+                        <input class="my-input" type="text" name="search" placeholder="Search">
+                        <button class="btn btn-primary" type="submit">Search</button>
+                    </form>
+                    <form action="{{route('comics.index')}}" method="GET" class="my-2">
+                        <button class="btn btn-danger" type="submit">X</button>
+                    </form>
+                </div>
             </span>
             <div class="row">
                 @foreach ($comics as $comic)
